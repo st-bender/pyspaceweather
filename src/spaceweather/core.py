af0973bd93e455d51290d328e1ddc86dc8c707ff
Stefan Bender <stefan.bender@ntnu.no>
2020-04-06 19:35:51 +0200

    Fix software name
    Some copy-and-paste leftovers.
    
    [skip ci]

--- src/spaceweather/core.py
+++ src/spaceweather/core.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2020 Stefan Bender
 #
 # This module is part of pyspaceweather.
-# pynrlmsise00 is free software: you can redistribute it or modify
+# pyspaceweather is free software: you can redistribute it or modify
 # it under the terms of the GNU General Public License as published
 # by the Free Software Foundation, version 2.
 # See accompanying LICENSE file or http://www.gnu.org/licenses/gpl-2.0.html.
